f2d49896bc11825f9ba46656704a5f35ece39663
Jesse Vazquez <Jesse.Vazquez@trincoll.edu>
2014-09-29 19:37:46 -0400

    Rotate simple cipher
    Rotate by X amount

---
+++ rotX.py
@@ -0,0 +1,14 @@
+def rotx(input, amt):
+	"""	Variant of the ROT13 substitution cipher that can rotate by any amount 1-26. 
+		Takes in an string, and an integer amount to rotate by"""
+	lows = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	CAPS = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	rotated = ""
+	for i in input:
+		if i in lows:
+			rotated += lows[(lows.index(i)+amt) % 26]
+		elif i in CAPS:
+			rotated += CAPS[(CAPS.index(i)+amt) % 26]
+		else:
+			rotated += i 
+	return rotated
